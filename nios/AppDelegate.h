ecb5c23164c6450cf2ae89e7c9e176b2094af825
Sebastian Waisbrot <seppo0010@gmail.com>
2012-02-04 10:11:49 -0300

    Initial Commit

---
+++ nios/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  nios
+//
+//  Created by Sebastian Waisbrot on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ nios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  nios
+//
+//  Created by Sebastian Waisbrot on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
