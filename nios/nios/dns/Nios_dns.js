58900d3ef111e3d2cf31b4f5f563b3390f59c709
Sebastian Waisbrot <seppo0010@gmail.com>
2012-02-09 17:25:17 -0300

    Fix DNS lookup with no family specified

--- nios/nios/dns/Nios_dns.js
+++ nios/nios/dns/Nios_dns.js
@@ -1,9 +1,10 @@
 exports.lookup = function (domain, family, callback) {
 	var args;
 	if (arguments.length === 2) {
-		args = [domain, callback];
+		args = [domain];
+		callback = family;
 	} else {
-		args = [domain, family, callback];
+		args = [domain, family];
 	}
 	Nios_call("Nios_dns", "lookup", args, callback);
 }
