3c0b41e53ee5a849a0162a8cddc98ed26bbcc23f
Sebastian Waisbrot <seppo0010@gmail.com>
2012-02-15 12:00:52 -0300

    Add ios binding for vibrate; Unable to test callback so it's temporarily commented

---
+++ nios/nios/ios/Nios_ios.m
@@ -0,0 +1,13 @@
+//
+//  Nios_ios.m
+//  nios
+//
+//  Created by Sebastian Waisbrot on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Nios_ios.h"
+
+@implementation Nios_ios
+
+@end
