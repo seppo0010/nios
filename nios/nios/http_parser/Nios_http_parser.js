a443dc236a9c8eb2c4063dc275045d0a314282c7
Sebastian Waisbrot <seppo0010@gmail.com>
2012-02-23 22:20:09 -0300

    Fix usage of HTTPParser.finish; it was reserved to be called manually on 'ondata', using inline function to replace the parsing of the c results

--- nios/nios/http_parser/Nios_http_parser.js
+++ nios/nios/http_parser/Nios_http_parser.js
@@ -6,21 +6,23 @@ exports.HTTPParser.REQUEST = 0
 exports.HTTPParser.RESPONSE = 1
 
 exports.HTTPParser.prototype.execute = function (d, start, length) {
-	Nios_call("Nios_http_parser", "execute", [ this.type, buffer_to_string(d), start, length ], this.finish.bind(this), true);
+	var self = this;
+	Nios_call("Nios_http_parser", "execute", [ this.type, buffer_to_string(d), start, length ], function (params) {
+		var messages = params[0];
+		for (var i = 0; i < messages.length; i++) {
+			var event = messages[i][0];
+			if (self[event]) {
+				self[event].apply(self, messages[i].length >= 2 ? messages[i][1] : null);
+			}
+		}
+	}, true);
 	// TODO: doing this sync because of a "race condition".
 	// It seems like the http.ondata is being called and
 	// expect to have parser.incoming already with the data
 	// after calling parser.execute
 }
 
-exports.HTTPParser.prototype.finish = function (params) {
-	var messages = params[0];
-	for (var i = 0; i < messages.length; i++) {
-		var event = messages[i][0];
-		if (this[event]) {
-			this[event].apply(this, messages[i].length >= 2 ? messages[i][1] : null);
-		}
-	}
+exports.HTTPParser.prototype.finish = function () {
 }
 
 exports.HTTPParser.prototype.reinitialize = function () {
