8eb1448f2dc7b64c23d568a088f3f8c362d2ea0b
Sebastian Waisbrot <seppo0010@gmail.com>
2012-02-28 18:31:35 -0300

    Showing IP and port when starting example to be more user-friendly

--- nios/example/ViewController.m
+++ nios/example/ViewController.m
@@ -7,8 +7,38 @@
 //
 
 #import "ViewController.h"
+#import <ifaddrs.h>
+#import <arpa/inet.h>
 
 @implementation ViewController
+
+// Get IP Address
+- (NSString *)getIPAddress {    
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];               
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+	
+}
 
 - (void) viewDidLoad {
 	[super viewDidLoad];
@@ -44,6 +74,7 @@
 			if ([[params objectAtIndex:0] isEqual:@"listening"]) {
 				[startStopButton setTitle:@"Stop" forState:UIControlStateNormal];
 				listening = TRUE;
+				[[[[UIAlertView alloc] initWithTitle:@"Now listening..." message:[NSString stringWithFormat:@"Open a browser pointing to http://%@:8080/", [self getIPAddress]] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] autorelease] show];
 			}
 		}
 	}
